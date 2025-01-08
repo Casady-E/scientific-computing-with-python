ce95ae12fb062421fc6fb973fc79d9166929e492
Casady-E <ethancasady1@gmail.com>
2025-01-07 22:25:00 -0700

    Create a for loop that cycles through the text variable and locates their corresponding position in the alphabet variable

--- ex-01.py
+++ ex-01.py
@@ -5,3 +5,9 @@ index = alphabet.find(text[0].lower())
 print(index)
 shifted = alphabet[index + shift]
 print(shifted)
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
+    new_char = alphabet[new_index]
