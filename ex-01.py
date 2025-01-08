0291be8e955825b3ec2191bcabc0f1c99708f086
Casady-E <ethancasady1@gmail.com>
2025-01-07 23:18:21 -0700

    Change for loop into the caesar function that takes message and offset as the argument. Check for spaces with the if else statement

--- ex-01.py
+++ ex-01.py
@@ -1,17 +1,37 @@
-text = 'Hello World'
+#text = 'Hello World'
+#shift = 3
+#alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#index = alphabet.find(text[0].lower())
+#print(index)
+#shifted = alphabet[index + shift]
+#print(shifted)
+
+#encrypted_text = ''
+
+#for char in text.lower():
+#    if char == ' ':
+#       encrypted_text += char
+#    index = alphabet.find(char)
+#    new_index = index + shift
+#    encrypted_text += alphabet[new_index]
+#    print('char:', char, 'encrypted text:', encrypted_text)
+
+text = 'Hello Zaira'
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0].lower())
-print(index)
-shifted = alphabet[index + shift]
-print(shifted)
 
-encrypted_text = ''
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
 
-for char in text.lower():
-    if char == ' ':
-        encrypted_text += char
-    index = alphabet.find(char)
-    new_index = index + shift
-    encrypted_text += alphabet[new_index]
-    print('char:', char, 'encrypted text:', encrypted_text)
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+
+caesar(text, shift)
+caesar(text, 13)
