c108cdda1a26851e8144cdcf771132208fecaa56
Casady-E <ethancasady1@gmail.com>
2025-01-08 00:42:02 -0700

    create two new functions for encrypt and decrypt to simplify the code and allow for extra arguments. Add in default arguments for these functions. Create a way to check if the text has special characters

--- building-a-cipher/ex-02.py
+++ building-a-cipher/ex-02.py
@@ -1,20 +1,21 @@
-text = 'Hello Zaira'
+text = 'Hello Zaira!'
 custom_key = 'python'
 
-def vigenere(message, key, direction):
+def vigenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     final_message = ''
 
     for char in message.lower():
-    
-        # Append space to the message
-        if char == ' ':
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
             final_message += char
         else:        
             # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
+
             # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
@@ -22,8 +23,14 @@ def vigenere(message, key, direction):
             final_message += alphabet[new_index]
     
     return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
     
-encryption = vigenere(text, custom_key, 1)
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+    
+encryption = encrypt(text, custom_key)
 print(encryption)
-decryption = vigenere(encryption, custom_key, -1)
+decryption = decrypt(encryption, custom_key)
 print(decryption)
